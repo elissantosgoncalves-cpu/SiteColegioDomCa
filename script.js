480a90d0c7c4ca11b5fde80b9e857e14b4d7a134
elissantosgoncalves-cpu <elis.santos.goncalves@escola.pr.gov.br>
2026-04-06 11:04:53 -0300

    salvar

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 document.addEventListener("DomContentLoaded",function(){
-//Corrige o ul da agenda caso a classe nao esteja certa
-const secaoAgenda = document.querySelector("#agenda");
+const secaoAgenda= document. querySelector("agenda");
+if (secaoAgenda) {
+const ulAgenda= secaoAgenda.querySelector("ul");
+   if (ulAgenda) {
+   ulAgenda.classList.add("agenda");
+   }
+   }
 
+const secaoContato = document.querySelector("#contato");
+if(secaoContato) {
+secaoContato.classList.add(contato-box);
+}
 
+ seleciona links do menu
+const linksMenu = document.querySelector("nav a");
+const secoes= document.querySelectorAll("section[id]");
+
+ Deixa a transição suave nos cards via js
+const cards= document.querySelectorAll (".card");
+for(let i = 0; i < cards. lenghth; i++){
+cards[i].computedstyle.transition = "0.3s ease";
+
+cards[i].adddEvenList("mouseenter", function(){
+this.style.transform = "translateY(-6px)";
 });
+
+cards[i].adddEvenListener("mouseenter", function(){
+this.style.transform = "translateY(0)";
+});
+ }
+});
+
